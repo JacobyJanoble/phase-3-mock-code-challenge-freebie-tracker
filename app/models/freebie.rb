8d1ee3d3f147422fcf947bfcfbadabc6f48936e0
Jacob <Jacob.McCann214@gmail.com>
2022-05-24 10:47:18 -0500

    Completed Assignment

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,10 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :comapny
+    belongs_to :dev
+
+    def print_details
+        "#{dev.name} got a #{item_name} from #{company.name} booth over there!"
+    end
+
 
 end
